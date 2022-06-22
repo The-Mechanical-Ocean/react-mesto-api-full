5103c4538028909ed90559356b0133dbc74f635a
The-Mechanical-Ocean <alex.kudrin@bk.ru>
2022-06-22 10:59:40 +0300

    "feat: убрал храние в куках"

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -119,11 +119,7 @@ module.exports.login = (req, res, next) => {
   User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
-      res.cookie('jwt', token, {
-        maxAge: 604800000,
-        httpOnly: true,
-        sameSite: true,
-      }).send({ data: token });
+      res.send({ token });
     })
     .catch((err) => {
       if (err.name === 'Error') {
