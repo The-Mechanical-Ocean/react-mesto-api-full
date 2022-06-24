8c34d432e2f7399fcd48c3e951d1fc1a7af02ca9
The-Mechanical-Ocean <alex.kudrin@bk.ru>
2022-06-24 14:31:54 +0300

    "feat: front fix"

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -72,8 +72,8 @@ function App() {
       history.push('/');
       Promise.all([api.getInitialCards(), api.getProfile()])
         .then(([resCards, resUser]) => {
-          setCards(resCards);
-          setCurrentUser(resUser);
+          setCards(resCards.cards);
+          setCurrentUser(resUser.user);
         })
         .catch((err) =>{console.log(`Ошибка: ${err}`)})        
     }
